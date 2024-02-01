766c4df836cb823774fbdc8a9c519d3a25935f9a
Sachin Kumar Behera <sachin.behera@attryb.com>
2024-02-01 12:41:11 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bada-box">
+        <div className="box1 item">Header</div>
+        <div className="box2 item">Sidebar</div>
+        <div className="box3 item">Content1</div>
+        <div className="box4 item">Content2</div>
+        <div className="box5 item">Content3</div>
+        <div className="box6 item">Footer</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
