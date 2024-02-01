4ecc5463bf17d303bede6fb7600c6f296b660a73
Sachin Kumar Behera <sachin.behera@attryb.com>
2024-02-01 12:52:36 +0530

    font change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,9 @@ function App() {
       <div className="bada-box">
         <div className="box1 item">Header</div>
         <div className="box2 item">Sidebar</div>
-        <div className="box3 item">Content1</div>
-        <div className="box4 item">Content2</div>
-        <div className="box5 item">Content3</div>
+        <div className="box3 item">Content 1</div>
+        <div className="box4 item">Content 2</div>
+        <div className="box5 item">Content 3</div>
         <div className="box6 item">Footer</div>
       </div>
     </div>
